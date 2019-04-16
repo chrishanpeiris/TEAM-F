bd23d17cb001f0ba95798819397e928abcb985c7
chrishanpeiris <chrishanmx@gmail.com>
2019-04-17 00:50:46 +0930

    Added form template pattern to refactor the code.

--- S311764_Chrishan_Peiris/Form_Template_Method-Task2/After/Game.java
+++ S311764_Chrishan_Peiris/Form_Template_Method-Task2/After/Game.java
@@ -1,41 +1,79 @@
+//Chrishan Peiris - S311764 - Group F
 
-class Cricket {
-   void startPlay() {
-      System.out.println("Cricket Game Started. Enjoy the game!");
+//add an abstract class to define the template
+abstract class GameTemplate {
+   abstract void initialize();//define abstract methods
+   abstract void startPlay();//define abstract methods
+   abstract void endPlay();//define abstract methods
+
+   //create template method - used final keyword to keep it as a constant template - used void keyword as it does not have any return type
+   final void play(){
+
+      //create the order for methods to be run - based on the defined template order, the methods will be executed
+
+      //initialize the game
+      initialize();
+
+      //start game
+      startPlay();
+
+      //end game
+      endPlay();
    }
-   void initialize() {
-      System.out.println("Cricket Game Initialized! Start playing.");
-   }
+}
+
+ //retrive the features from super class "GameTemplate" to sub class "Cricket" using extends keyword
+ class Cricket extends GameTemplate {
+
+   //overide the already defined common methods as we want in sub class
+   @Override
    void endPlay() {
       System.out.println("Cricket Game Finished!");
    }
- }
 
- class Football {
-   void startPlay() {
-      System.out.println("Football Game Started. Enjoy the game!");
-   }
+   //overide the already defined common methods as we want in sub class
+   @Override
    void initialize() {
-      System.out.println("Football Game Initialized! Start playing.");
+      System.out.println("Cricket Game Initialized! Start playing.");
    }
+
+   //overide the already defined common methods as we want in sub class
+   @Override
+   void startPlay() {
+      System.out.println("Cricket Game Started. Enjoy the game!");
+   }
+}
+
+ //retrive the features from super class "GameTemplate" to sub class "Football" using extends keyword
+ class Football extends GameTemplate {
+
+   //overide the already defined common methods as we want in sub class
+   @Override
    void endPlay() {
       System.out.println("Football Game Finished!");
    }
- }
 
- public class Game {
+   //overide the already defined common methods as we want in sub class
+   @Override
+   void initialize() {
+      System.out.println("Football Game Initialized! Start playing.");
+   }
+
+   //overide the already defined common methods as we want in sub class
+   @Override
+   void startPlay() {
+      System.out.println("Football Game Started. Enjoy the game!");
+   }
+}
+
+public class Game {
+   //initialize main method
    public static void main(String[] args) {
 
-      Cricket cricket = new Cricket();
-      cricket.startPlay();
-      cricket.initialize();
-      cricket.endPlay();
-
+      GameTemplate game = new Cricket(); // create an object of GameTemplate class and assign to new instance of Cricket class
+      game.play(); // using the created GameTemplate object call the method inside Cricket class
       System.out.println();
-
-      Football football = new Football();
-      football.startPlay();		
-      football.initialize();
-      football.endPlay();
+      game = new Football(); // reassign the GameTemplate object to new instance of Football class
+      game.play();// using the created GameTemplate object call the method inside Football class		
    }
 }
