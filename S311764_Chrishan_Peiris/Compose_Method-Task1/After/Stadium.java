be0607fecca66d967213620a05cd31e1020d3f90
chrishanpeiris <chrishanmx@gmail.com>
2019-04-18 16:41:46 +0930

    Added compose method to refactor the code.

--- S311764_Chrishan_Peiris/Compose_Method-Task1/After/Stadium.java
+++ S311764_Chrishan_Peiris/Compose_Method-Task1/After/Stadium.java
@@ -6,20 +6,38 @@ class TicketPriceCalculate {
 
   public double getTicketPrice(int quantity) {
     double charge;
-    if (quantity >= 5 && quantity <= 100) {
-      charge = quantity * summerRate;
+
+    // move the if condition into a seperate method called isQuantity
+    if (isQuantity(quantity)) {
+      charge = summerCharge(quantity); // move summercharge calculation into a seperate method called summerCharge
     } else {
-      charge = quantity * winterRate + winterServiceCharge;
+      charge = winterCharge(quantity); // move winterCharge calculation into a seperate method called winterCharge
     }
     return charge;
   }
+
+  //isQuantity conditional method implementation
+  private boolean isQuantity (int quantity) {
+    return quantity >= 5 && quantity <= 100;
+  }
+
+  //summerCharge calculation method implentation
+  private double summerCharge (int quantity) {
+    return quantity * summerRate;
+  }
+
+  //winterCharge calculation method implementation
+  private double winterCharge(int quantity) {
+    return quantity * winterRate + winterServiceCharge;
+  }
+
 }
 
 class Stadium {
   public static void main(String[] args) {
       Double ticketPrice;
-      TicketPriceCalculate ticket = new TicketPriceCalculate();
-      ticketPrice = ticket.getTicketPrice(10);
+      TicketPriceCalculate ticket = new TicketPriceCalculate(); // create an Object of TicketPriceCalculate class 
+      ticketPrice = ticket.getTicketPrice(10); // call the method using create object and pass 10 as the parameter
       System.out.println(ticketPrice);
   }
 }
