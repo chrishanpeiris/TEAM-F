0c74bb6b92e14e16d1559728139597d161981a66
chrishanpeiris <chrishanmx@gmail.com>
2019-04-18 16:18:44 +0930

    Added main method and make it as a working example.

--- S311764_Chrishan_Peiris/Compose_Method-Task1/Before/Stadium.java
+++ S311764_Chrishan_Peiris/Compose_Method-Task1/Before/Stadium.java
@@ -1,17 +1,25 @@
-class Stadium {
+class TicketPriceCalculate {
   // ...
-  public double summerRate;
-  public double winterRate;
-  public double winterServiceCharge;
+  public double summerRate = 5;
+  public double winterRate = 10;
+  public double winterServiceCharge = 2;
 
-  public double getTicketPrice(Date date, int quantity) {
+  public double getTicketPrice(int quantity) {
     double charge;
-    if (date.before(WINTER_START) || date.after(WINTER_END)) {
+    if (quantity >= 5 && quantity <= 100) {
       charge = quantity * summerRate;
-    }
-    else {
+    } else {
       charge = quantity * winterRate + winterServiceCharge;
     }
     return charge;
+  }
+}
+
+class Stadium {
+  public static void main(String[] args) {
+      Double ticketPrice;
+      TicketPriceCalculate ticket = new TicketPriceCalculate();
+      ticketPrice = ticket.getTicketPrice(10);
+      System.out.println(ticketPrice);
   }
 }
