5fd14f35e02ac74a7e10be1b6c12a36a8526f573
DanthakaSri <pasinduworks92@gmail.com>
2019-04-19 15:33:08 +0930

    Task 6
    Descriptors package before

---
+++ S310750_Pasindu_Sandanayke/Encapsulate_Classes_with_Factory-Task6/Before/Descriptor.java
@@ -0,0 +1,15 @@
+package descriptors;
+public abstract class AttributeDescriptor...
+protected AttributeDescriptor(...)
+public class BooleanDescriptor extends AttributeDescriptor...
+public BooleanDescriptor(...) {
+super(...);
+}
+public class DefaultDescriptor extends AttributeDescriptor...
+public DefaultDescriptor(...) {
+super(...);
+}
+public class ReferenceDescriptor extends AttributeDescriptor...
+public ReferenceDescriptor(...) {
+super(...);
+}
