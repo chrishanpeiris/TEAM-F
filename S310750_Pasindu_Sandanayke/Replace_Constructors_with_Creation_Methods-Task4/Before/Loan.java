363ff9c2353c96e23e61cc0c6599528a92649473
DanthakaSri <pasinduworks92@gmail.com>
2019-04-19 16:17:47 +0930

    final changes for task 4
    all files comitted to relevant folders

--- S310750_Pasindu_Sandanayke/Replace_Constructors_with_Creation_Methods-Task4/Before/Loan.java
+++ S310750_Pasindu_Sandanayke/Replace_Constructors_with_Creation_Methods-Task4/Before/Loan.java
@@ -1,40 +1,34 @@
-	public class Loan{
-
-		//changed access modifire and made it catch-call class
-		private Loan(CapitalStrategy capitalStrategy, double commitment,
-		int riskRating, Date maturity, Date expiry) {
-			this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
-		}
-		//Change constructor into static creational method and rename createTermLoan
-		public static Loan createTermLoan(double commitment, int riskRating, Date maturity) {
-			return new Loan(commitment, 0.00, riskRating, maturity, null);
-		}
-		//Change constructor into static creational method and rename createTermLoan
-		public static Loan createTermLoan(double commitment, int riskRating, Date maturity, Date expiry) {
-			this(commitment, 0.00, riskRating, maturity, expiry);
-		}
-		//Change constructor into static creational method and rename CreateRTCL
-		public static Loan CreateRTCL(double commitment, double outstanding,
-		int customerRating, Date maturity, Date expiry) {
-			this(null, commitment, outstanding, customerRating, maturity, expiry);
-		}	
-		//Change constructor into static creational method and rename CreateRTCL
-		public static Loan CreateRTCL(CapitalStrategy capitalStrategy, double commitment,
-		double outstanding, int riskRating,
-		Date maturity, Date expiry) {
-			this.commitment = commitment;
-			this.outstanding = outstanding;
-			this.riskRating = riskRating;
-			this.maturity = maturity;
-			this.expiry = expiry;
-			this.capitalStrategy = capitalStrategy;
-			if (capitalStrategy == null) {
-				if (expiry == null)
-				this.capitalStrategy = new CapitalStrategyTermLoan();
-				else if (maturity == null)
-				this.capitalStrategy = new CapitalStrategyRevolver();
-				else
-				this.capitalStrategy = new CapitalStrategyRCTL();
-			}
+public class Loan{
+	public Loan(double commitment, int riskRating, Date maturity) {
+		this(commitment, 0.00, riskRating, maturity, null);
+	}
+	public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
+		this(commitment, 0.00, riskRating, maturity, expiry);
+	}
+	public Loan(double commitment, double outstanding,
+	int customerRating, Date maturity, Date expiry) {
+		this(null, commitment, outstanding, customerRating, maturity, expiry);
+	}
+	public Loan(CapitalStrategy capitalStrategy, double commitment,
+	int riskRating, Date maturity, Date expiry) {
+		this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
+	}
+	public Loan(CapitalStrategy capitalStrategy, double commitment,
+	double outstanding, int riskRating,
+	Date maturity, Date expiry) {
+		this.commitment = commitment;
+		this.outstanding = outstanding;
+		this.riskRating = riskRating;
+		this.maturity = maturity;
+		this.expiry = expiry;
+		this.capitalStrategy = capitalStrategy;
+		if (capitalStrategy == null) {
+			if (expiry == null)
+			this.capitalStrategy = new CapitalStrategyTermLoan();
+			else if (maturity == null)
+			this.capitalStrategy = new CapitalStrategyRevolver();
+			else
+			this.capitalStrategy = new CapitalStrategyRCTL();
 		}
 	}
+}
