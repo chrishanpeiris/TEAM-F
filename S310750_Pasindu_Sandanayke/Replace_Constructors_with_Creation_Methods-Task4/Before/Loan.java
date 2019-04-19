0b47f94f6060836f84ab7805b9b2d59bb1a40968
DanthakaSri <pasinduworks92@gmail.com>
2019-04-19 16:10:21 +0930

    Task 4 completed with code
    Refactored with creational method

--- S310750_Pasindu_Sandanayke/Replace_Constructors_with_Creation_Methods-Task4/Before/Loan.java
+++ S310750_Pasindu_Sandanayke/Replace_Constructors_with_Creation_Methods-Task4/Before/Loan.java
@@ -1,20 +1,25 @@
 	public class Loan{
 
-		public Loan(double commitment, int riskRating, Date maturity) {
-			this(commitment, 0.00, riskRating, maturity, null);
-		}
-		public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
-			this(commitment, 0.00, riskRating, maturity, expiry);
-		}
-		public Loan(double commitment, double outstanding,
-		int customerRating, Date maturity, Date expiry) {
-			this(null, commitment, outstanding, customerRating, maturity, expiry);
-		}
-		public Loan(CapitalStrategy capitalStrategy, double commitment,
+		//changed access modifire and made it catch-call class
+		private Loan(CapitalStrategy capitalStrategy, double commitment,
 		int riskRating, Date maturity, Date expiry) {
 			this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
 		}
-		public Loan(CapitalStrategy capitalStrategy, double commitment,
+		//Change constructor into static creational method and rename createTermLoan
+		public static Loan createTermLoan(double commitment, int riskRating, Date maturity) {
+			return new Loan(commitment, 0.00, riskRating, maturity, null);
+		}
+		//Change constructor into static creational method and rename createTermLoan
+		public static Loan createTermLoan(double commitment, int riskRating, Date maturity, Date expiry) {
+			this(commitment, 0.00, riskRating, maturity, expiry);
+		}
+		//Change constructor into static creational method and rename CreateRTCL
+		public static Loan CreateRTCL(double commitment, double outstanding,
+		int customerRating, Date maturity, Date expiry) {
+			this(null, commitment, outstanding, customerRating, maturity, expiry);
+		}	
+		//Change constructor into static creational method and rename CreateRTCL
+		public static Loan CreateRTCL(CapitalStrategy capitalStrategy, double commitment,
 		double outstanding, int riskRating,
 		Date maturity, Date expiry) {
 			this.commitment = commitment;
