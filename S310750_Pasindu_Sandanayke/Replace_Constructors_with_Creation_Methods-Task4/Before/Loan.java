a7435b13c3e656505391b2401a74a02183800cbc
DanthakaSri <pasinduworks92@gmail.com>
2019-04-19 13:15:39 +0930

    Task 4
    before replace construsctors with creation methods

---
+++ S310750_Pasindu_Sandanayke/Replace_Constructors_with_Creation_Methods-Task4/Before/Loan.java
@@ -0,0 +1,35 @@
+	public class Loan{
+
+		public Loan(double commitment, int riskRating, Date maturity) {
+			this(commitment, 0.00, riskRating, maturity, null);
+		}
+		public Loan(double commitment, int riskRating, Date maturity, Date expiry) {
+			this(commitment, 0.00, riskRating, maturity, expiry);
+		}
+		public Loan(double commitment, double outstanding,
+		int customerRating, Date maturity, Date expiry) {
+			this(null, commitment, outstanding, customerRating, maturity, expiry);
+		}
+		public Loan(CapitalStrategy capitalStrategy, double commitment,
+		int riskRating, Date maturity, Date expiry) {
+			this(capitalStrategy, commitment, 0.00, riskRating, maturity, expiry);
+		}
+		public Loan(CapitalStrategy capitalStrategy, double commitment,
+		double outstanding, int riskRating,
+		Date maturity, Date expiry) {
+			this.commitment = commitment;
+			this.outstanding = outstanding;
+			this.riskRating = riskRating;
+			this.maturity = maturity;
+			this.expiry = expiry;
+			this.capitalStrategy = capitalStrategy;
+			if (capitalStrategy == null) {
+				if (expiry == null)
+				this.capitalStrategy = new CapitalStrategyTermLoan();
+				else if (maturity == null)
+				this.capitalStrategy = new CapitalStrategyRevolver();
+				else
+				this.capitalStrategy = new CapitalStrategyRCTL();
+			}
+		}
+	}
