61b9a3495d7a3aed658f9b0c90cf64fae80b9beb
chrishanpeiris <chrishanmx@gmail.com>
2019-05-02 01:05:01 +0930

    Refactored the code by renaming the method

--- S311764_Chrishan_Peiris/Rename_Method_Task11/After/Person.java
+++ S311764_Chrishan_Peiris/Rename_Method_Task11/After/Person.java
@@ -1,6 +1,8 @@
 class PersonDetails {
-    //…
-    public String getTpNum() {
+    
+    // provide a meaningful name to the method
+    // since the varibles are related to office details, it is possible to provide a meaningful name as mentioned below
+    public String getOfficeTelephoneNumber() {
       String officeAreaCode = "084";
       String officeNumber = "55566855";
       return ("(" + officeAreaCode + ") " + officeNumber);
@@ -12,7 +14,7 @@ class PersonDetails {
 public class Person {
     public static void main(String[] args) {
         PersonDetails person = new PersonDetails();
-        String details = person.getTpNum();
+        String details = person.getOfficeTelephoneNumber();// call the renamed method
         System.out.println(details);
     }
 }
