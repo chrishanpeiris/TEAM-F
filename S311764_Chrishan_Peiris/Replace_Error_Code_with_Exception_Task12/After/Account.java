92ee3b1c57dc61c162ee46d042a04caf2f07f2f3
chrishanpeiris <chrishanmx@gmail.com>
2019-05-02 22:39:31 +0930

    Refactored the code by applying try catch exception handling

--- S311764_Chrishan_Peiris/Replace_Error_Code_with_Exception_Task12/After/Account.java
+++ S311764_Chrishan_Peiris/Replace_Error_Code_with_Exception_Task12/After/Account.java
@@ -4,20 +4,28 @@ class AccountInfo {
 
     int withdraw(int amount) {
         if (amount > balance) {
-          return -1;
+            return -1;
+        } else {
+            balance -= amount;
+            return 0;
         }
-        else {
-          balance -= amount;
-          return 0;
-        }
-      }
-  }
+    }
+}
 
-  class Account {
-      public static void main(String[] args) {
-          AccountInfo account = new AccountInfo();
-          int withdrawAuthorisation = account.withdraw(1500);
-          System.out.println(withdrawAuthorisation);
-      }
-  }
-  
+class Account {
+    public static void main(String[] args) {
+
+        AccountInfo account = new AccountInfo();// created an object instance of AccountInfo class
+
+        // add try catch methods handle exceptions
+        try {
+            int withdrawAuthorisation = account.withdraw(50 / 0);
+            System.out.println(withdrawAuthorisation);
+        }
+        // catch the exeception and handle it as we want > here it will show an error
+        // message rather than java exception
+        catch (Exception e) {
+            System.out.println("Something went wrong.");
+        }
+    }
+}
