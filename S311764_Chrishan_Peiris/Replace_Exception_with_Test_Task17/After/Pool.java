701e81f8893974b8fc943d924ec03fab3fbd13b3
chrishanpeiris <chrishanmx@gmail.com>
2019-05-09 16:19:31 +0930

    Refactored using Replace_Exception_with_Test_ method

--- S311764_Chrishan_Peiris/Replace_Exception_with_Test_Task17/After/Pool.java
+++ S311764_Chrishan_Peiris/Replace_Exception_with_Test_Task17/After/Pool.java
@@ -1,10 +1,14 @@
 class ResourcePool {
 
     public void getResource() {
-        try {
-            int[] myNumbers = { 1, 2, 3 };
-            System.out.println(myNumbers[10]);
-        } catch (Exception e) {
+        int[] myNumbers = { 1, 2, 3 }; //define the array
+        int position = 3;
+        
+        // use if else insted of using try catch - check the condition using if
+        if (position <= 2) { 
+            System.out.println(myNumbers[position]);
+        }
+        else {
             System.out.println("Something went wrong.");
         }
 
@@ -13,7 +17,7 @@ class ResourcePool {
 
 public class Pool {
     public static void main(String[] args) {
-        ResourcePool resourcepool = new ResourcePool();
-        resourcepool.getResource();
+        ResourcePool resourcepool = new ResourcePool(); // create an instance of ResourcePool object
+        resourcepool.getResource(); // call the method via created instance
     }
 }
