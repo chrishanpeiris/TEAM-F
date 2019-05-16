547afe3ebb245c941d03b0046138e7852ee81c62
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-05-16 23:48:56 +0930

    Update code

--- S308078_Shih-Liang_Fang/Bridge_Pattern_Taks52/Before/Whole cdoe_before.java
+++ S308078_Shih-Liang_Fang/Bridge_Pattern_Taks52/Before/Whole cdoe_before.java
@@ -53,6 +53,14 @@ public class Test {
         System.out.println("---- Bag's brief introduction ----");
         fewViewBag.photo();
         fewViewBag.snippet();
+        FullViewBag fullViewBag = new FullViewBag();
+        System.out.println("---- Bag's full description ----");
+        fullViewBag.photo();
+        fullViewBag.describe();
+        FewViewBook fewViewBook = new FewViewBook();
+        System.out.println("---- Book's brief introduction ----");
+        fewViewBook.photo();
+        fewViewBook.snippet();
         FullViewBook fullViewBook = new FullViewBook();
         System.out.println("---- Book's full description ----");
         fullViewBook.photo();
