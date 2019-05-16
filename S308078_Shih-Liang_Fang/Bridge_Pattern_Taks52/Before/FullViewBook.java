d78a9e82abde0af1af98a6f484cba75aa6d0bc65
Shih Liang Fang <37144635+haihau@users.noreply.github.com>
2019-05-16 23:47:10 +0930

    Grooming Task52

---
+++ S308078_Shih-Liang_Fang/Bridge_Pattern_Taks52/Before/FullViewBook.java
@@ -0,0 +1,11 @@
+public class FullViewBook {
+
+    public void photo() {
+        System.out.println("Book's picture");
+    }
+
+
+    public void describe() {
+        System.out.println("Book's full introduction");
+    }
+}
---
+++ S308078_Shih-Liang_Fang/Bridge_Pattern_Taks52/Before/Whole cdoe_before.java
@@ -0,0 +1,62 @@
+public class FewViewBag {
+
+
+    public void photo() {
+        System.out.println("Bag's picture");
+    }
+
+
+    public void snippet() {
+        System.out.println("Bag's brief introduction");
+    }
+
+}
+
+public class FewViewBook {
+    public void photo() {
+        System.out.println("Book's picture");
+    }
+
+
+    public void snippet() {
+        System.out.println("Book's brief introduction");
+    }
+}
+
+public class FullViewBag {
+    public void photo() {
+        System.out.println("Bag's picture");
+    }
+
+
+    public void describe() {
+        System.out.println("Bag's full introduction");
+    }
+}
+
+public class FullViewBook {
+
+    public void photo() {
+        System.out.println("Book's picture");
+    }
+
+
+    public void describe() {
+        System.out.println("Book's full introduction");
+    }
+}
+public class Test {
+    @org.junit.jupiter.api.Test
+    public void test(){
+
+        FewViewBag fewViewBag = new FewViewBag();
+        System.out.println("---- Bag's brief introduction ----");
+        fewViewBag.photo();
+        fewViewBag.snippet();
+        FullViewBook fullViewBook = new FullViewBook();
+        System.out.println("---- Book's full description ----");
+        fullViewBook.photo();
+        fullViewBook.describe();
+
+    }
+}
